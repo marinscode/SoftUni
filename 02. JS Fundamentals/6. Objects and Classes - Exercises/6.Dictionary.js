b3cd51b8e1c756489af6f9ce84d5b60a86ce8fcd
Marin Totev <marin.totev@gmail.com>
2021-06-06 18:27:32 +0100

    Objects and Classes Exercises

---
+++ 02. JS Fundamentals/6. Objects and Classes - Exercises/6.Dictionary.js
@@ -0,0 +1,25 @@
+function solve(arr) {
+    let dictionary = {};
+
+    for (let line of arr) {
+        let parsedLine = JSON.parse(line);
+        let key = Object.keys(parsedLine);
+
+        dictionary[key] = parsedLine[key];
+
+    }
+
+    let sortedKeys = Object.keys(dictionary).sort((a, b) => a.localeCompare(b));
+
+    for (let key of sortedKeys) {
+        console.log(`Term: ${key} => Definition: ${dictionary[key]}`);
+    }
+}
+
+solve([
+    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+]);
