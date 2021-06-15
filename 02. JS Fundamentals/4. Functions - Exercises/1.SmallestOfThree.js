8bc146e9c78852dca626b0ff43a7b7912e54248a
Marin Totev <marin.totev@gmail.com>
2021-06-15 21:42:33 +0100

    updated 1st exercise

--- 02. JS Fundamentals/4. Functions - Exercises/1.SmallestOfThree.js
+++ 02. JS Fundamentals/4. Functions - Exercises/1.SmallestOfThree.js
@@ -1,11 +1,12 @@
 function smallestNum(a, b, c) {
-    if (a <= b && a <= c) {
-        console.log(a);
-    } else if (b <= a && b <= c) {
-        console.log(b);
-    } else if (c <= a && c <= b) {
-        console.log(c);
-    }
+    // if (a <= b && a <= c) {
+    //     console.log(a);
+    // } else if (b <= a && b <= c) {
+    //     console.log(b);
+    // } else if (c <= a && c <= b) {
+    //     console.log(c);
+    // }
+    return Math.min(a, b, c);
 }
 
 smallestNum(2,
